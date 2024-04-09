e81bfd85c3ad3a969c95782b82e888beb771e48f
habib motavassel <habib.motavassel@gmail.com>
2024-04-09 15:43:37 +0200

    second version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,114 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import { View } from './components/View';
+
+const sounds = [
+  {
+      id: 'Q',
+      keyName: 'Heater 1',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3',
+      keycode: 81,
+  },
+  {
+      id: 'W',
+      keyName: 'Heater 2',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3',
+      keycode: 87,
+  },
+  {
+      id: 'E',
+      keyName: 'Heater 3',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3',
+      keycode: 69,
+  },
+  {
+      id: 'A',
+      keyName: 'Heater 4',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3',
+      keycode: 65,
+  },
+  {
+      id: 'S',
+      keyName: 'Clap',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3',
+      keycode: 83,
+  },
+  {
+      id: 'D',
+      keyName: 'Open-HH',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3',
+      keycode: 68,
+  },
+  {
+      id: 'Z',
+      keyName: 'Kick-n\'-Hat',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3',
+      keycode: 90,
+  },
+  {
+      id: 'X',
+      keyName: 'Kick',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3',
+      keycode: 88,
+  },
+  {
+      id: 'C',
+      keyName: 'Closed-HH',
+      path: 'https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3',
+      keycode: 67,
+  }
+];
+
 function App() {
+
+  const [state, setState] = useState({
+    isOn: false,
+    keyName: '',
+    volume: 50,
+    soundName: '',
+  });
+
+
+  const powerHandker = ({target}) => {
+    setState(prevState => ({...prevState, isOn:!state.isOn}))
+
+    if (!state.isOn) {
+      target.classList.add("activeBtn");
+    } else {
+      target.classList.remove("activeBtn");
+    }
+  }
+
+
+  const volumeHandler = ({target}) => {
+    setState(prevState => ({...prevState, volume:target.value}));
+  }
+
+
+  const setSoundName =(name) => {
+    setState(prevState => ({...prevState, soundName:name}))
+    setTimeout(() => {
+      setState(prevState => ({...prevState, soundName:''}))
+    }, 1500)
+    };
+
+
+
+  const padClickHandler = ({target}) => {
+    if (state.isOn) {
+    const sound = target.firstElementChild;
+    sound.volume = state.volume / 100;
+    sound.play();
+
+    setSoundName(target.name);
+    }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <View value={{padClickHandler, sounds, powerHandker, volumeHandler, state, setSoundName}} />
+    </React.Fragment>
   );
 }
 
---
+++ src/components/View.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from 'react';
+
+import icon from '../images/free-code-camp.svg'
+
+const Pad = (props) => {
+
+    const [state, setState] = useState("")
+
+
+    const keyDownHandler = (e) => {
+
+        if(e.keyCode === props.pad.keycode) {
+          const sound = document.getElementById(props.pad.id);
+          sound.volume = props.value.state.volume / 100;
+          sound.play()
+
+          props.value.setSoundName(props.pad.keyName)
+        }
+    }
+
+
+    useEffect(() => {
+        if (props.value.state.isOn) {
+            document.addEventListener('keydown', keyDownHandler)
+
+        }
+            return () => {document.removeEventListener('keydown', keyDownHandler)}
+    })
+
+    const className = props.value.state.isOn ? `drum-pad activeDrumPad ${state}`:  "drum-pad" ;
+
+    return (
+        <button
+            className= {className}
+            onClick={props.value.padClickHandler}
+            name={props.pad.keyName}
+        >
+            {props.children}
+            <audio src={props.pad.path} id={props.pad.id} className="clip"></audio>
+        </button>
+    );
+};
+
+export const View = ({value}) => {
+
+    const pads = value.sounds.map(item => <Pad key={item.id} value= {value} pad={item}>{item.id}</Pad>)
+    const soundVolume = value.state.isOn ? "sound-volume" : undefined;
+
+    return (
+        <>
+            <div id="drum-machine" className="container">
+                <section id="display">
+                    <div style={{height: `${value.state.volume}%`}} id={soundVolume}></div>
+                    <span>{value.state.soundName}</span>
+                </section>
+                <section id="volumes">
+                    <div className="volume"><input type="range" min="0" max="80" value={value.state.volume} onChange={value.volumeHandler} /></div>
+                    <div className="volume"><input type="range" disabled></input></div>
+                </section>
+                <section id="buttons">
+                    <button onClick={value.powerHandker}>Power</button>
+                    <span> FFC <img src={icon} alt="freeCoCamp logo"/></span>
+                </section>
+                <section id="pads">
+                    {pads}
+                </section>
+            </div>
+        </>
+    );
+};
